547dcf63bed2e98276bfa715552dfa6007f400c0
Tecnoprofedavid <103004579+Tecnoprofedavid@users.noreply.github.com>
2022-04-10 01:38:39 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,44 @@
 input.onButtonPressed(Button.A, function () {
-    if (posicionpaleta > 0) {
+    if (paleta1.get(LedSpriteProperty.X) > 0) {
         paleta1.move(-1)
         paleta2.move(-1)
-        posicionpaleta += -1
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
 input.onButtonPressed(Button.B, function () {
-    if (posicionpaleta < 3) {
+    if (paleta1.get(LedSpriteProperty.X) < 3) {
         paleta1.move(1)
         paleta2.move(1)
-        posicionpaleta += 1
     }
 })
 function moverpelota () {
-    basic.pause(500)
+    basic.pause(400)
     pelota.move(1)
     pelota.ifOnEdgeBounce()
     if (pelota.isTouching(paleta1) || pelota.isTouching(paleta2)) {
-        pelota.change(LedSpriteProperty.Direction, randint(45, -45))
-        puntos += 1
-    } else {
-        if (pelota.get(LedSpriteProperty.Y) == 4) {
-            basic.clearScreen()
-            basic.showIcon(IconNames.No)
-            basic.showString("ptos")
-            basic.showNumber(puntos)
-        }
+        pelota.change(LedSpriteProperty.Direction, 0 + randint(45, -45))
+        game.addScore(1)
+    }
+    if (pelota.get(LedSpriteProperty.Y) == 4 && !(pelota.isTouching(paleta1) || pelota.isTouching(paleta2))) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.No)
+        game.removeLife(1)
     }
 }
-let puntos = 0
-let posicionpaleta = 0
 let pelota: game.LedSprite = null
 let paleta2: game.LedSprite = null
 let paleta1: game.LedSprite = null
 paleta1 = game.createSprite(2, 4)
 paleta2 = game.createSprite(3, 4)
 pelota = game.createSprite(2, 0)
-posicionpaleta = 2
-puntos = 0
-pelota.turn(Direction.Right, 90)
+game.setLife(3)
+game.setScore(0)
+pelota.change(LedSpriteProperty.Direction, 180 + randint(45, -45))
 basic.forever(function () {
     moverpelota()
+    if (0 < 1) {
+    	
+    }
 })
