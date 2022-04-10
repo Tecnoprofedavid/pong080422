3469e42a6fa53c61f0ecf1dcc6b1a9054b741434
Tecnoprofedavid <103004579+Tecnoprofedavid@users.noreply.github.com>
2022-04-10 10:27:55 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,14 @@ input.onButtonPressed(Button.A, function () {
         paleta2.move(-1)
     }
 })
+function posición_inicial () {
+    paleta1 = game.createSprite(2, 3)
+    paleta2 = game.createSprite(3, 3)
+    pelota = game.createSprite(2, 0)
+    pelota.change(LedSpriteProperty.Direction, 270 + randint(45, -45))
+}
 input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.SmallDiamond)
     control.reset()
 })
 input.onButtonPressed(Button.B, function () {
@@ -17,28 +24,41 @@ function moverpelota () {
     basic.pause(400)
     pelota.move(1)
     pelota.ifOnEdgeBounce()
-    if (pelota.isTouching(paleta1) || pelota.isTouching(paleta2)) {
-        pelota.change(LedSpriteProperty.Direction, 0 + randint(45, -45))
-        game.addScore(1)
+    if (pelota.isTouching(paleta1)) {
+        pelota.change(LedSpriteProperty.Direction, 180 + randint(0, 45))
+        Puntos += 1
     }
-    if (pelota.get(LedSpriteProperty.Y) == 4 && !(pelota.isTouching(paleta1) || pelota.isTouching(paleta2))) {
-        basic.clearScreen()
-        basic.showIcon(IconNames.No)
-        game.removeLife(1)
+    if (pelota.isTouching(paleta2)) {
+        pelota.change(LedSpriteProperty.Direction, 180 + randint(-45, 0))
+        Puntos += 1
+    }
+    if (pelota.get(LedSpriteProperty.Y) > 3) {
+        basic.pause(500)
+        Vidas += -1
+        basic.showIcon(IconNames.Triangle)
+        pelota.delete()
+        paleta1.delete()
+        paleta2.delete()
+        posición_inicial()
     }
 }
 let pelota: game.LedSprite = null
 let paleta2: game.LedSprite = null
 let paleta1: game.LedSprite = null
-paleta1 = game.createSprite(2, 4)
-paleta2 = game.createSprite(3, 4)
-pelota = game.createSprite(2, 0)
-game.setLife(3)
-game.setScore(0)
-pelota.change(LedSpriteProperty.Direction, 180 + randint(45, -45))
+let Vidas = 3
+let Puntos = 0
+posición_inicial()
 basic.forever(function () {
     moverpelota()
-    if (0 < 1) {
-    	
+    if (Vidas < 1) {
+        pelota.delete()
+        paleta1.delete()
+        paleta2.delete()
+        basic.showIcon(IconNames.Chessboard)
+        basic.showString("Game over")
+        basic.showString("Puntos")
+        for (let index = 0; index < 2; index++) {
+            basic.showString("" + (Puntos))
+        }
     }
 })
